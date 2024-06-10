c24a1c513c8b5363cc44f1b973243a686ccdfe8b
ahmed mohamed <ahmed.mohamed91196@gmail.com>
2024-06-10 21:15:24 +0300

    fix importing modules & handling getenv variables

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """Creates status route"""
 from api.v1.views import app_views
-from models import storage
+from flask import jsonify
 
 
 @app_views.route("/status")
 def get_status():
     """Returns status"""
-    return {"status": "OK"}
+    return jsonify({"status": "OK"})
 
 
 @app_views.route("/api/v1/stats")
 def get_stats():
     """Returns stats"""
+    from models import storage
     return storage.count()

62a91713707473fc5197fe4e1af817f79ecb0993
ahmed mohamed <ahmed.mohamed91196@gmail.com>
2024-06-10 20:48:26 +0300

    handle retrieving the number of each objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 """Creates status route"""
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route("/status")
 def get_status():
     """Returns status"""
     return {"status": "OK"}
+
+
+@app_views.route("/api/v1/stats")
+def get_stats():
+    """Returns stats"""
+    return storage.count()

3d36ac6fa92ccf17afe885e141897ae3cb3f1ee2
ahmed mohamed <ahmed.mohamed91196@gmail.com>
2024-06-10 21:31:03 +0300

    fix get_stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,4 +14,18 @@ def get_status():
 def get_stats():
     """Returns stats"""
     from models import storage
-    return storage.count()
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+    classes = {
+        'Amenity': storage.count(Amenity),
+        'City': storage.count(City),
+        'Place': storage.count(Place),
+        'Review': storage.count(Review),
+        'State': storage.count(State),
+        'User': storage.count(User)
+    }
+    return jsonify(classes)

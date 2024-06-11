81fc5ec3417652871ffdfe4aa32dc0209bef8c42
ahmed mohamed <ahmed.mohamed91196@gmail.com>
2024-06-12 01:38:14 +0300

    handle CORS access

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
 """starts a Flask web application"""
 import os
-
 from flask import Flask, jsonify, make_response
-
+from flask_cors import CORS
 from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
 def close_session(exception=None):
     """Closes session"""
     from models import storage
+
     storage.close()
 
 
 @app.errorhandler(404)
 def not_found(error):
-    """ handles 404 error and gives json formatted response """
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    """handles 404 error and gives json formatted response"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
